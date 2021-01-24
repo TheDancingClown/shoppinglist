416963b21b2ec47f008f90deaffeadaf6df5ee39
Louis Kirkham <chew@talk21.com>
2021-01-24 22:16:49 +0000

    detox init

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [searchValue, setSearch] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter an ingredient</Text>
+      <TextInput
+      onChangeText={text => setSearch(text)}
+      style={{borderColor: 'black', borderWidth: 1, width: 200}}
+      />
+      <Text>{searchValue}</Text>
       <StatusBar style="auto" />
     </View>
   );
