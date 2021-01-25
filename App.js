07d90642236a7445f41345110ead49d14a46cf55
Louis Kirkham <chew@talk21.com>
2021-01-25 20:13:40 +0000

    rebuild for ios and passing first e2e test

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import { StyleSheet, Text, TextInput, View, FlatList, SafeAreaView } from 'react-native';
 import Meal from './components/Meal'
 import MealFinder from './src/MealFinder'
 
@@ -8,29 +8,31 @@ export default function App() {
   const recipes = require('./src/recipes.json')
   const [meals, setMeals] = useState(recipes)
   const planner = new MealFinder();
+
   const renderMeals = ({ item }) => (
     <Meal key={item.index} title={item.title} />
   );
 
-  const searchIngredients = async (text) => {
+  const searchIngredients = (text) => {
     setMeals(planner.filterMatchingMeals(text, recipes));
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Enter an ingredient</Text>
       <TextInput
       onChangeText={text => searchIngredients(text)}
       style={{borderColor: 'lightgray', borderWidth: 1, width: 200}}
       />
       <FlatList
+        testID='recipes'
         data={meals}
         renderItem={renderMeals}
         extraData={meals}
         keyExtractor={item => item.key}
       />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -40,6 +42,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20
+    paddingTop: 20
   },
 });
